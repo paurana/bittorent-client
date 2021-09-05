e9167a0599d57479c7a584f3eb667444c576fcdf
Aayush Paurana <75433620+tech-yush@users.noreply.github.com>
2021-09-06 04:12:26 +0530

    established tcp conn with peers

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/tech-yush/bittorent-client/bencode"
 	"github.com/tech-yush/bittorent-client/peers"
+	"github.com/tech-yush/bittorent-client/tcp"
 )
 
 func main() {
@@ -17,5 +19,12 @@ func main() {
 	}
 	resp, _ := bencode.ParseResp(torrentfile)
 	peers, _ := peers.SeparatePeers([]byte(resp.Peers))
-	fmt.Println(peers)
+	// fmt.Println(peers)
+	var peerID [20]byte
+	_, _ = rand.Read(peerID[:])
+	conn, err := tcp.New(peers[0], peerID, torrentfile.InfoHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(conn)
 }
